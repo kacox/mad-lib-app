b6ed2c74b850e291dd448004dac0e9625b342a7c
Kasey <ka.cox@outlook.com>
2018-06-02 21:48:40 -0700

    Initial commit (skeleton for app)

---
+++ mad-lib-app.py
@@ -0,0 +1,11 @@
+"""
+This is a mad lib app made using Flask.
+
+`mad-lib-app.py` defines the Flask application instance and includes a few
+tasks that help manage the control flow of the application.
+"""
+# imports
+from app_main import create_app
+
+# create flask application instance
+app = create_app()
